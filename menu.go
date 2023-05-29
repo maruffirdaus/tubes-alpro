e7c57347f966426d3db9d55cef3a8f683604f048
farizrjanuar <farizrjanuar@gmail.com>
2023-05-29 13:11:00 +0700

    edit activity

--- menu.go
+++ menu.go
@@ -427,6 +427,14 @@ func changeActivityMenu() {
 	fmt.Print("\n")
 	fmt.Print("*---------- UBAH KEGIATAN -----------*\n")
 	printHeader()
+	fmt.Print("|   Pilih Hari:                      |\n")
+	fmt.Print("|                                    |\n")
+	fmt.Print("|      1. Senin                      |\n")
+	fmt.Print("|      2. Selasa                     |\n")
+	fmt.Print("|      3. Rabu                       |\n")
+	fmt.Print("|      4. Kamis                      |\n")
+	fmt.Print("|      5. Jumat                      |\n")
+	fmt.Print("|                                    |\n")
 	fmt.Print("|   Menu Lain:                       |\n")
 	fmt.Print("|                                    |\n")
 	fmt.Print("|      9. Kembali                    |\n")
@@ -435,6 +443,23 @@ func changeActivityMenu() {
 
 	fmt.Print("Pilih nomor menu: ")
 	fmt.Scan(&selectedMenu)
+
+	if selectedMenu == "1" {
+		fmt.Printf("\x1bc")
+		changeSelectTimeMenu(0)
+	} else if selectedMenu == "2" {
+		fmt.Printf("\x1bc")
+		changeSelectTimeMenu(1)
+	} else if selectedMenu == "3" {
+		fmt.Printf("\x1bc")
+		changeSelectTimeMenu(2)
+	} else if selectedMenu == "4" {
+		fmt.Printf("\x1bc")
+		changeSelectTimeMenu(3)
+	} else if selectedMenu == "5" {
+		fmt.Printf("\x1bc")
+		changeSelectTimeMenu(4)
+	}
 
 	if selectedMenu == "9" {
 		fmt.Printf("\x1bc")
@@ -445,6 +470,61 @@ func changeActivityMenu() {
 	}
 }
 
+func changeSelectTimeMenu(day int) {
+	fmt.Print("\n")
+	fmt.Print("*----------- UBAH KEGIATAN ----------*\n")
+	printHeader()
+	fmt.Print("|   Pilih Waktu:                     |\n")
+	fmt.Print("|                                    |\n")
+	fmt.Print("|      a. 08.00 - 09.00              |\n")
+	fmt.Print("|      b. 09.00 - 10.00              |\n")
+	fmt.Print("|      c. 10.00 - 11.00              |\n")
+	fmt.Print("|      d. 11.00 - 12.00              |\n")
+	fmt.Print("|      e. 12.00 - 13.00              |\n")
+	fmt.Print("|      f. 13.00 - 14.00              |\n")
+	fmt.Print("|      g. 14.00 - 15.00              |\n")
+	fmt.Print("|      h. 15.00 - 16.00              |\n")
+	fmt.Print("|      i. 16.00 - 17.00              |\n")
+	fmt.Print("|                                    |\n")
+	fmt.Print("|   Menu Lain:                       |\n")
+	fmt.Print("|                                    |\n")
+	fmt.Print("|      9. Kembali                    |\n")
+	fmt.Print("|                                    |\n")
+	fmt.Print("*------------------------------------*\n\n")
+
+	fmt.Print("Pilih nomor menu: ")
+	fmt.Scan(&selectedMenu)
+
+	if selectedMenu == "a" {
+		fmt.Printf("\x1bc")
+		changeActivity(day, 0)
+	} else if selectedMenu == "b" {
+		fmt.Printf("\x1bc")
+		changeActivity(day, 1)
+	} else if selectedMenu == "c" {
+		fmt.Printf("\x1bc")
+		changeActivity(day, 2)
+	} else if selectedMenu == "d" {
+		fmt.Printf("\x1bc")
+		changeActivity(day, 3)
+	} else if selectedMenu == "e" {
+		fmt.Printf("\x1bc")
+		changeActivity(day, 4)
+	} else if selectedMenu == "f" {
+		fmt.Printf("\x1bc")
+		changeActivity(day, 5)
+	} else if selectedMenu == "g" {
+		fmt.Printf("\x1bc")
+		changeActivity(day, 6)
+	} else if selectedMenu == "h" {
+		fmt.Printf("\x1bc")
+		changeActivity(day, 7)
+	} else if selectedMenu == "i" {
+		fmt.Printf("\x1bc")
+		changeActivity(day, 8)
+	}
+
+}
 func deleteActivityMenu() {
 	/*
 		I.S. -
@@ -478,6 +558,22 @@ func deleteActivityMenu() {
 
 	fmt.Print("Pilih nomor menu: ")
 	fmt.Scan(&selectedMenu)
+	if selectedMenu == "1" {
+		fmt.Printf("\x1bc")
+		deleteSelectTimeMenu(0)
+	} else if selectedMenu == "2" {
+		fmt.Printf("\x1bc")
+		deleteSelectTimeMenu(1)
+	} else if selectedMenu == "3" {
+		fmt.Printf("\x1bc")
+		deleteSelectTimeMenu(2)
+	} else if selectedMenu == "4" {
+		fmt.Printf("\x1bc")
+		deleteSelectTimeMenu(3)
+	} else if selectedMenu == "5" {
+		fmt.Printf("\x1bc")
+		deleteSelectTimeMenu(4)
+	}
 
 	if selectedMenu == "6" {
 		fmt.Printf("\x1bc")
